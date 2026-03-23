deb0afbb10382df0a6f7f705fc8a9f6f46c61e0f
emmanuelk3 <emmanuel.k@tecnolynx.com>
2026-03-23 13:27:42 +0530

    HTML Added

--- master/urls.py
+++ master/urls.py
@@ -5,4 +5,8 @@ app_name = 'master'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('recommendations/', views.recommendations, name='recommendations'),
+    path('cost-budget/', views.cost_budget, name='cost_budget'),
+    path('swept-path/', views.swept_path, name='swept_path'),
+    path('analysis/', views.analysis, name='analysis'),
 ]
--- master/views.py
+++ master/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'master/dashboard.html')
+
+def recommendations(request):
+    return render(request, 'master/recommendations.html')
+
+def cost_budget(request):
+    return render(request, 'master/cost_budget.html')
+
+def swept_path(request):
+    return render(request, 'master/swept_path.html')
+
+def analysis(request):
+    return render(request, 'master/analysis.html')
